172863853a83543cc5136ef91926c4d51287d24a
Usama <pmtam2010@gmail.com>
2020-06-14 22:00:24 +0930

    First commit

---
+++ ImportCsvToDatabase/Helpers/SqlHelper.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace ImportCsvToDatabase.Helpers
+{
+    public static class SqlHelper
+    {
+        public static string ExecuteProcedureReturnString(string connString, string procName, params SqlParameter[] paramters)
+        {
+            var result = "";
+            using (var sqlConnection = new SqlConnection(connString))
+            {
+                using (var command = sqlConnection.CreateCommand())
+                {
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.CommandText = procName;
+                    if (paramters != null)
+                    {
+                        command.Parameters.AddRange(paramters);
+                    }
+                    try
+                    {
+                        sqlConnection.Open();
+                        var ret = command.ExecuteScalar();
+                        if (ret != null)
+                        {
+                            result = Convert.ToString(ret);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        return "";
+                    }
+                }
+            }
+            return result;
+        }
+
+        ///Methods to get values of   
+        ///individual columns from sql data reader  
+        #region Get Values from Sql Data Reader  
+        public static string GetNullableString(SqlDataReader reader, string colName)
+        {
+            return reader.IsDBNull(reader.GetOrdinal(colName)) ? null : Convert.ToString(reader[colName]);
+        }
+
+        public static int GetNullableInt32(SqlDataReader reader, string colName)
+        {
+            return reader.IsDBNull(reader.GetOrdinal(colName)) ? 0 : Convert.ToInt32(reader[colName]);
+        }
+
+        public static bool GetBoolean(SqlDataReader reader, string colName)
+        {
+            return reader.IsDBNull(reader.GetOrdinal(colName)) ? default(bool) : Convert.ToBoolean(reader[colName]);
+        }
+
+        //this method is to check wheater column exists or not in data reader  
+        public static bool IsColumnExists(this System.Data.IDataRecord dr, string colName)
+        {
+            try
+            {
+                return (dr.GetOrdinal(colName) >= 0);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        #endregion
+    }
+}
---
+++ ImportCsvToDatabase/Program.cs
@@ -0,0 +1,67 @@
+﻿using ImportCsvToDatabase.Helpers;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ImportCsvToDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Server=DESKTOP-2DOEOSC\\MSSQLSERVER01;Database=inex;Trusted_Connection=True;MultipleActiveResultSets=True";
+            string filepath = "Test Data/purchases.csv";
+            DataTable dt = ConvertCSVtoDataTable(filepath);
+            foreach (DataRow dataRow in dt.Rows)
+            {
+                var outParam = new SqlParameter("@ReturnCode", System.Data.SqlDbType.NVarChar, 20)
+                {
+                    Direction = System.Data.ParameterDirection.Output
+                };
+
+                SqlParameter[] parameters =
+                        {
+                            new SqlParameter("@ID", dataRow.ItemArray.GetValue(0).ToString()),
+                            new SqlParameter("@contact_id", dataRow.ItemArray.GetValue(1).ToString()),
+                            new SqlParameter("@product_id", dataRow.ItemArray.GetValue(2).ToString()),
+                            new SqlParameter("@purchase_date", dataRow.ItemArray.GetValue(3).ToString()),
+                            outParam
+                        };
+                SqlHelper.ExecuteProcedureReturnString(connectionString, "InsertPurchases", parameters);
+                switch ((string)outParam.Value)
+                {
+                    case "C200":
+                        break;
+                    case "C201":
+                        break;
+                    case "C202":
+                        break;
+                }
+            }
+        }
+
+        private static DataTable ConvertCSVtoDataTable(string strFilePath)
+        {
+            StreamReader sr = new StreamReader(strFilePath);
+            var headers = sr.ReadLine().Split(',');
+            DataTable dt = new DataTable();
+            foreach(var header in headers)
+            {
+                dt.Columns.Add(header);
+            }
+            while(!sr.EndOfStream)
+            {
+                string[] rows = Regex.Split(sr.ReadLine(), ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+                DataRow row = dt.NewRow();
+                for (int i = 0; i < dt.Columns.Count; i++)
+                {
+                    row[i] = rows[i];
+                }
+                dt.Rows.Add(row);
+            }
+            return dt;
+        }
+    }
+}
